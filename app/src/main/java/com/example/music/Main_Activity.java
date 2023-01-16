fce6f63b4edc3cc59e87062bc2a5ae53e3e28bdd
Joy Mridha <joymridha2004@gmail.com>
2023-01-17 00:08:47 +0530

    Music_Listener_V_0.0.01 Application Begineer Level Ready

---
+++ app/src/main/java/com/example/music/Main_Activity.java
@@ -0,0 +1,88 @@
+package com.example.music;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Main_Activity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    TextView noMusicTextView;
+    ArrayList<AudioModel> Songlist = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recycler_view);
+        noMusicTextView = findViewById(R.id.no_song_text);
+
+        if (checkPermission() == false) {
+            requestPermission();
+            return;
+        }
+
+        String[] projection = {
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.DURATION,
+        };
+
+        String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
+        Cursor cursor = getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection, selection, null, null);
+        while (cursor.moveToNext()) {
+            AudioModel songData = new AudioModel(cursor.getString(1), cursor.getString(0), cursor.getString(2));
+            if (new File(songData.getPath()).exists()) {
+                Songlist.add(songData);
+            }
+
+        }
+
+        if (Songlist.size() == 0) {
+            noMusicTextView.setVisibility(View.VISIBLE);
+        } else {
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            recyclerView.setAdapter(new musicListAdavter(Songlist, getApplicationContext()));
+        }
+
+    }
+
+    boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(Main_Activity.this, READ_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(Main_Activity.this, READ_EXTERNAL_STORAGE)) {
+            Toast.makeText(Main_Activity.this, "READ PERMISSION IS REQUIRED,PLEASE ALLOW FROM SETTINGS", Toast.LENGTH_SHORT);
+        } else {
+            ActivityCompat.requestPermissions(Main_Activity.this, new String[]{READ_EXTERNAL_STORAGE}, 123);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (recyclerView != null) {
+            recyclerView.setAdapter(new musicListAdavter(Songlist, getApplicationContext()));
+        }
+    }
+}
--- app/src/main/java/com/example/music/musicListAdavter.java
+++ app/src/main/java/com/example/music/musicListAdavter.java
@@ -15,10 +15,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class musicListAdavter extends RecyclerView.Adapter<musicListAdavter.ViewHolder>{
+public class musicListAdavter extends RecyclerView.Adapter<musicListAdavter.ViewHolder> {
 
 
-    ArrayList<AudioModel>songsList;
+    ArrayList<AudioModel> songsList;
     Context context;
 
     public musicListAdavter(ArrayList<AudioModel> songsList, Context context) {
@@ -27,29 +27,29 @@ public class musicListAdavter extends RecyclerView.Adapter<musicListAdavter.View
     }
 
     @Override
-    public ViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(context).inflate(R.layout.recycler_item,parent,false);
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recycler_item, parent, false);
         return new musicListAdavter.ViewHolder(view);
     }
 
     @SuppressLint("ResourceAsColor")
     @Override
     public void onBindViewHolder(ViewHolder holder, @SuppressLint("RecyclerView") int position) {
-        AudioModel songData=songsList.get(position);
+        AudioModel songData = songsList.get(position);
         holder.titleTextView.setText(songData.getTitle());
 
-       if (MyMediaPlayer.currentIndex==position){
-           holder.titleTextView.setTextColor(Color.parseColor("#ff0000"));
-       }else{
-           holder.titleTextView.setTextColor(Color.parseColor("#000000"));
-       }
+        if (MyMediaPlayer.currentIndex == position) {
+            holder.titleTextView.setTextColor(Color.parseColor("#ff0000"));
+        } else {
+            holder.titleTextView.setTextColor(Color.parseColor("#000000"));
+        }
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 MyMediaPlayer.getInstance().reset();
-                MyMediaPlayer.currentIndex =position;
-                Intent intent= new Intent(context,MusicPlayerActivity.class);
-                intent.putExtra("List",songsList);
+                MyMediaPlayer.currentIndex = position;
+                Intent intent = new Intent(context, MusicPlayerActivity.class);
+                intent.putExtra("List", songsList);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(intent);
             }
@@ -61,14 +61,15 @@ public class musicListAdavter extends RecyclerView.Adapter<musicListAdavter.View
         return songsList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView titleTextView;
         ImageView iconImageView;
-        public ViewHolder( View itemView) {
+
+        public ViewHolder(View itemView) {
             super(itemView);
-            titleTextView =itemView.findViewById(R.id.music_title_text);
-            iconImageView=itemView.findViewById(R.id.icon_view);
+            titleTextView = itemView.findViewById(R.id.music_title_text);
+            iconImageView = itemView.findViewById(R.id.icon_view);
         }
     }
 }
